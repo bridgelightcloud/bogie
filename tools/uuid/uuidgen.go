bcab5b9c1169c6c4995e8c3c8737e660277dfef4
Seanny Phoenix <seannyphoenix@gmail.com>
2024-06-05 01:41:40 +0000

    uuid generator cli

---
+++ tools/uuid/uuidgen.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	count := os.Args[1]
+
+	if count == "" {
+		count = "1"
+	}
+
+	c, err := strconv.Atoi(count)
+
+	if err != nil {
+		fmt.Println("Usage: uuidgen [count]")
+	}
+
+	for i := 0; i < c; i++ {
+		uuid := uuid.New()
+		fmt.Println(uuid.String())
+		var arr [16]byte = uuid
+		fmt.Printf("%#v\n\n", arr)
+	}
+}
