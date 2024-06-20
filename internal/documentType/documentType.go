d9785cf32eaa1f11932521c7d1bb62032e446fe5
Seanny Phoenix <seannyphoenix@gmail.com>
2024-06-20 13:17:47 -0700

    add user document type

--- internal/documentType/documentType.go
+++ internal/documentType/documentType.go
@@ -4,12 +4,14 @@ import "github.com/google/uuid"
 
 const (
 	Event = "event"
-	User               = "user"
+	Note  = "note"
+	User  = "user"
 )
 
 var NameMap = map[string]uuid.UUID{
 	Event: uuid.MustParse("88c2333e-2bc2-4063-b865-719c24211d2c"),
-	User:               uuid.MustParse("8d930d82-24e9-4fc1-824b-9e1253d4ee02"),
+	Note:  uuid.MustParse("22322bb8-a869-43b2-9991-8466fd02a19b"),
+	User:  uuid.MustParse("8d930d82-24e9-4fc1-824b-9e1253d4ee02"),
 }
 
 var IDMap map[uuid.UUID]string
