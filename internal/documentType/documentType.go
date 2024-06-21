c41b08f14373fabea5765d60bd22e790262e5694
Seanny Phoenix <seannyphoenix@gmail.com>
2024-06-21 04:12:02 +0000

    scrap document type Note

--- internal/documentType/documentType.go
+++ internal/documentType/documentType.go
@@ -4,13 +4,11 @@ import "github.com/google/uuid"
 
 const (
 	Event = "event"
-	Note  = "note"
 	User  = "user"
 )
 
 var NameMap = map[string]uuid.UUID{
 	Event: uuid.MustParse("88c2333e-2bc2-4063-b865-719c24211d2c"),
-	Note:  uuid.MustParse("22322bb8-a869-43b2-9991-8466fd02a19b"),
 	User:  uuid.MustParse("8d930d82-24e9-4fc1-824b-9e1253d4ee02"),
 }
 
