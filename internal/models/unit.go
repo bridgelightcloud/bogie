8fd876a3533904350ec7d86807364617e5697033
Seanny Phoenix <seannyphoenix@gmail.com>
2024-10-04 16:34:43 +0000

    marshal/unmarshal unit<>dynamnodb

--- internal/models/unit.go
+++ internal/models/unit.go
@@ -1,8 +1,10 @@
 package models
 
 import (
+	"strconv"
 	"time"
 
+	dynamodb "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/bridgelightcloud/bogie/internal/db"
 	"github.com/google/uuid"
 )
@@ -33,9 +35,91 @@ func GetExampleUnit() Unit {
 }
 
 func (u Unit) MarshalDynamoDB() (DBDocument, error) {
-	return nil, nil
+	if u.Id == uuid.Nil {
+		return nil, db.ErrBadDocID
+	}
+
+	data := DBDocument{
+		db.ID: &dynamodb.AttributeValueMemberB{Value: u.Id[:]},
+	}
+
+	if id, ok := db.NameMap[u.Type]; ok {
+		data[db.Type] = &dynamodb.AttributeValueMemberB{Value: id[:]}
+	} else {
+		return nil, db.ErrBadDocType
+	}
+
+	if id, ok := db.NameMap[u.Status]; ok {
+		data[db.Status] = &dynamodb.AttributeValueMemberB{Value: id[:]}
+	} else {
+		return nil, db.ErrBadDocStatus
+	}
+
+	if u.CreatedAt != nil && !u.CreatedAt.IsZero() {
+		data[db.CreatedAt] = &dynamodb.AttributeValueMemberN{
+			Value: strconv.FormatInt(u.CreatedAt.Unix(), 10),
+		}
+	} else {
+		return nil, db.ErrBadCreatedAt
+	}
+
+	if u.UpdatedAt != nil && !u.UpdatedAt.IsZero() {
+		data[db.UpdatedAt] = &dynamodb.AttributeValueMemberN{
+			Value: strconv.FormatInt(u.UpdatedAt.Unix(), 10),
+		}
+	} else {
+		return nil, db.ErrBadUpdatedAt
+	}
+
+	if u.Agency != "" {
+		data[db.Agency] = &dynamodb.AttributeValueMemberS{Value: u.Agency}
+	}
+
+	if u.UnitID != "" {
+		data[db.UnitID] = &dynamodb.AttributeValueMemberS{Value: u.Agency}
+	}
+
+	if len(u.Notes) > 0 {
+		data[db.Notes] = &dynamodb.AttributeValueMemberSS{Value: u.Notes}
+	}
+
+	return data, nil
 }
 
 func (u *Unit) UnmarshalDynamoDB(data DBDocument) error {
+	if id := db.GetUUID(data[db.ID]); id != uuid.Nil {
+		u.Id = id
+	} else {
+		return db.ErrBadDocID
+	}
+
+	if t, ok := db.IDMap[db.GetUUID(data[db.Type])]; ok {
+		u.Type = t
+	} else {
+		return db.ErrBadDocType
+	}
+
+	if s, ok := db.IDMap[db.GetUUID(data[db.Status])]; ok {
+		u.Status = s
+	} else {
+		return db.ErrBadDocStatus
+	}
+
+	if t := db.GetTime(data[db.CreatedAt]); !t.IsZero() {
+		u.CreatedAt = &t
+	} else {
+		return db.ErrBadCreatedAt
+	}
+
+	if t := db.GetTime(data[db.UpdatedAt]); !t.IsZero() {
+		u.UpdatedAt = &t
+	} else {
+		return db.ErrBadUpdatedAt
+	}
+
+	u.Agency = db.GetString(data[db.Agency])
+	u.UnitID = db.GetString(data[db.UnitID])
+	u.Notes = db.GetStringSlice(data[db.Notes])
+
 	return nil
 }
