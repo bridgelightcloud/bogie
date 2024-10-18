c44086e34b6ddf4692f82d0ec94d905f2b96c78c
Seanny Phoenix <seannyphoenix@gmail.com>
2024-10-18 21:49:54 +0000

    update fixtures

--- internal/fixtures/fixtures.go
+++ internal/fixtures/fixtures.go
@@ -6,12 +6,17 @@ import (
 	"github.com/google/uuid"
 )
 
+var testUUIDString = "8d930d82-24e9-4fc1-824b-9e1253d4ee02"
+
 var testTime = time.Now()
 
+// GetTestUUID returns a UUID equal to 8d930d82-24e9-4fc1-824b-9e1253d4ee02
 func GetTestUUID() uuid.UUID {
-	return uuid.MustParse("8d930d82-24e9-4fc1-824b-9e1253d4ee02")
+	return uuid.MustParse(testUUIDString)
 }
 
+// GetTestUUIDSlice returns a slce of size len composed of copies of
+// the test UUID 8d930d82-24e9-4fc1-824b-9e1253d4ee02
 func GetTestUUIDSlice(len int) []uuid.UUID {
 	uuids := make([]uuid.UUID, len)
 	for i := 0; i < len; i++ {
@@ -26,4 +31,16 @@ func GetTestTime() time.Time {
 
 func GetTestTimePtr() *time.Time {
 	return &testTime
+}
+
+// MaybeRefreshUUID checks if a UUID u is equal to uuid.NIL, and
+// if it is, it replaces the value with the value of a new UUID
+func MaybeRefreshUUID(u *uuid.UUID) {
+	if *u == uuid.Nil {
+		*u = uuid.New()
+	}
+}
+
+func IntPtr(i int) *int {
+	return &i
 }
