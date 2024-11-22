1614bb2fbb5fb2d530d76e8991d7bf6e9009eba9
Seanny Phoenix <seannyphoenix@gmail.com>
2024-11-22 22:03:12 +0000

    common unit tests

--- pkg/csvmum/common_test.go
+++ pkg/csvmum/common_test.go
@@ -73,3 +73,91 @@ func TestGetHeaderNamesToIndices(t *testing.T) {
 		})
 	}
 }
+
+func TestGetExportedName(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		name     string
+		input    any
+		expected string
+	}{{
+		name:     "unexported",
+		input:    struct{ one string }{},
+		expected: "",
+	}, {
+		name:     "exported",
+		input:    struct{ One string }{},
+		expected: "One",
+	}, {
+		name: "tagged",
+		input: struct {
+			One string `csv:"uno"`
+		}{},
+		expected: "uno",
+	}, {
+		name: "tagged multiple",
+		input: struct {
+			One string `csv:"uno,dos"`
+		}{},
+		expected: "uno",
+	}, {
+		name: "tagged empty",
+		input: struct {
+			One string `csv:""`
+		}{},
+		expected: "",
+	}}
+
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert := assert.New(t)
+
+			f := reflect.TypeOf(tc.input).Field(0)
+
+			name := getExportedName(f)
+			assert.Equal(tc.expected, name)
+		})
+	}
+}
+
+func TestGetOrderedHeaders(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		name     string
+		input    map[string]int
+		expected []string
+	}{{
+		name:     "empty",
+		input:    map[string]int{},
+		expected: []string{},
+	}, {
+		name:     "simple",
+		input:    map[string]int{"One": 0},
+		expected: []string{"One"},
+	}, {
+		name:     "complex",
+		input:    map[string]int{"One": 0, "Two": 1, "Three": 2},
+		expected: []string{"One", "Two", "Three"},
+	}, {
+		name:     "unordered",
+		input:    map[string]int{"Two": 2, "One": 0, "Three": 7},
+		expected: []string{"One", "Two", "Three"},
+	}}
+
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert := assert.New(t)
+
+			headers := getOrderedHeaders(tc.input)
+			assert.Equal(tc.expected, headers)
+		})
+	}
+}
