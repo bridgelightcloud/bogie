3924cdd659a5d0ee22855c90a019c5758297a589
Seanny Phoenix <seannyphoenix@gmail.com>
2024-11-22 22:02:48 +0000

    add getOrderedHeaders

--- pkg/csvmum/common.go
+++ pkg/csvmum/common.go
@@ -3,6 +3,7 @@ package csvmum
 import (
 	"fmt"
 	"reflect"
+	"sort"
 	"strings"
 )
 
@@ -12,7 +13,7 @@ func getHeaderNamesToIndices(t reflect.Type) (map[string]int, error) {
 	if t.Kind() != reflect.Struct {
 		return headers, fmt.Errorf("cannot get headers: not a struct")
 	}
-	for i := 0; i < t.NumField(); i++ {
+	for i := range t.NumField() {
 		f := t.Field(i)
 		if name := getExportedName(f); name != "" {
 			headers[name] = i
@@ -33,4 +34,17 @@ func getExportedName(f reflect.StructField) string {
 		}
 	}
 	return name
+}
+
+func getOrderedHeaders(hm map[string]int) []string {
+	hh := make([]string, 0, len(hm))
+	for n := range hm {
+		hh = append(hh, n)
+	}
+
+	sort.SliceStable(hh, func(i, j int) bool {
+		return hm[hh[i]] < hm[hh[j]]
+	})
+
+	return hh
 }
